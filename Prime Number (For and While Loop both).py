451879b99a10ec10c2ba5d32f55f81f891945051
Sameer Kazmi <sameerkazmi05@gmail.com>
2024-10-23 16:04:11 +0530

    Add files via upload

---
+++ Prime Number (For and While Loop both).py
@@ -0,0 +1,29 @@
+'''
+For and While loops used with else
+Ques) Write a program to check whether a number is prime.
+'''
+#  For Loop
+Num=int(input("Enter a number greater than 1: "))  #Taking an integer from user.
+List=[1]  #Using this list and asking the user till the user gives a positive integer.
+for i in List:  #Iterating the list.
+    if Num<=1:
+        Num=int(input("Enter a number greater than 1: "))
+        List.append(1)  #Adding one element to the list to again execute the for loop.
+for i in range(2,int(Num/2)+1):  #Iterating through numbers having a possibility of being a factor.
+    if Num%i==0:  #When a factor is encountered, the remainder would become 0.
+        print("Not Prime.")
+        break  #No need to check the remaining numbers if the number has already been proven not prime.
+else:
+    print("Prime.")  #This block is run when the whole for loop has been executed showing no possible factors.
+#  While Loop
+Num=int(input("Enter a number greater than 1: "))  #Taking an integer from the user.
+while Num<=1:  #Checking whether the number is not greater than 1.
+    Num=int(input("Enter a number greater than 1: "))  #Asking the user repeatedly till the user gives a number greater than 1.
+i=2  #Initializing the first number to be checked as a factor.
+while i<=int(i/2)+1:  #Till which all numbers are to be checked.
+    if Num%i==0:  #When a factor is encountered, the remainder is zero.
+        print("Not Prime.")
+        break  #No need to check the remaining numbers if the number has already been proven not prime.
+    i+=1  #Increasing by one to check the next numbers if it is a factor or not.
+else:
+    print("Prime.")
