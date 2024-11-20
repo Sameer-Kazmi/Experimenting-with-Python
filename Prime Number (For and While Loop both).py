846a85f6e13906aa90991f7cdb812979045d6b3e
Sameer Kazmi <sameerkazmi05@gmail.com>
2024-11-20 14:18:46 +0530

    Update Prime Number (For and While Loop both).py

--- Prime Number (For and While Loop both).py
+++ Prime Number (For and While Loop both).py
@@ -20,7 +20,7 @@ Num=int(input("Enter a number greater than 1: "))  #Taking an integer from the u
 while Num<=1:  #Checking whether the number is not greater than 1.
     Num=int(input("Enter a number greater than 1: "))  #Asking the user repeatedly till the user gives a number greater than 1.
 i=2  #Initializing the first number to be checked as a factor.
-while i<=int(i/2)+1:  #Till which all numbers are to be checked.
+while i<=int(Num/2)+1:  #Till which all numbers are to be checked.
     if Num%i==0:  #When a factor is encountered, the remainder is zero.
         print("Not Prime.")
         break  #No need to check the remaining numbers if the number has already been proven not prime.
